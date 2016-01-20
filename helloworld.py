c961f40c49a3c04dfacd75667e00a43e66fba573
Phillip Vo <pvo4@mail.valenciacollege.edu>
2016-01-19 19:35:39 -0500

    Week 2: Hello world

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,4 @@
-# replace the contents of this comment with your full name
+# Phillip Vo
 
 # write a program that:
 # 1. greets the user in English
@@ -8,3 +8,23 @@
 
 # make sure that your code contains comments explaining your logic!
 
+print('What is your name?')
+myName = input()
+print()
+print('It is a pleasure to meet you, ' + myName +'!')
+print()
+print('Choose a language and I will greet you in that language!')
+print('1. Russian')
+print('2. Japanese')
+print('3. Vietnamese')
+print()
+number = input()
+print()
+if number == '1':
+	print('Zdravstvuyte')
+elif number == '2':
+	print('Konnichiwa')
+elif number == '3':
+	print('Chao ban')
+else:
+	print('You did not input the correct number, try again.')
